fe5707cc1064f9f87fb30787857f1789e22a53c1
Sri Harsha Vardhan Kakumanu <harshakakumanu@users.noreply.github.com>
2020-08-18 07:14:25 -0700

    WIP

---
+++ app/core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    admin.sites.register(models.user)
+
+
